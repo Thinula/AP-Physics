41830821d1833da0c8c60f641cb55b8198dfeecd
Thinula <desil.tan@gmail.com>
2016-09-17 13:53:00 -0400

    Create Matrix.cpp

---
+++ Matrix/src/Matrix.cpp
@@ -0,0 +1,23 @@
+#include "Matrix.h"
+#include <iostream>
+
+using namespace std;
+
+//Matrix::Matrix()
+//{
+//    //ctor
+//}
+//
+//Matrix::~Matrix()
+//{
+//    //dtor
+//}
+
+int main()
+{
+    int r,c;
+    cout << "Enter the number of rows in your data set: ";
+    cin >> r;
+    cout << "Enter the number of columns in your data set: ";
+    cin >> c;
+}
